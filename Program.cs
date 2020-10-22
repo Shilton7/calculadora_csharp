739e303d49d00c4800b8f9d964a791eba4b71963
shilton <shilton.santiago@animaeducacao.com.br>
2020-10-22 09:45:41 -0300

    Multiplicação

--- Program.cs
+++ Program.cs
@@ -8,7 +8,8 @@ namespace calculadora_csharp
     {
       //Soma();
       //Substracao();
-      Divisao();
+      //Divisao();
+      Multiplicacao();
     }
 
     static void Soma()
@@ -60,6 +61,20 @@ namespace calculadora_csharp
       float resultado = num1 / num2;
       Console.WriteLine($"O resultado da divisão é: {resultado}");
       Console.ReadKey();
+    }
+
+    static void Multiplicacao()
+    {
+      Console.Clear();
+
+      Console.WriteLine("Digite o primeiro valor:");
+      float num1 = float.Parse(Console.ReadLine());
+
+      Console.WriteLine("Digite o segundo valor:");
+      float num2 = float.Parse(Console.ReadLine());
+
+      float resultado = num1 * num2;
+      Console.WriteLine($"O resultado da multiplicação é: {resultado}");
     }
   }
 }
