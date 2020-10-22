3ce53bd806a46815d82b19c9f6766a761fd00062
shilton <shilton.santiago@animaeducacao.com.br>
2020-10-22 10:19:20 -0300

    Menu da aplicação

--- Program.cs
+++ Program.cs
@@ -6,10 +6,36 @@ namespace calculadora_csharp
   {
     static void Main(string[] args)
     {
-      //Soma();
-      //Substracao();
-      //Divisao();
-      Multiplicacao();
+      Menu();
+    }
+
+    static void Menu()
+    {
+      Console.Clear();
+
+      Console.WriteLine("Bem vindo a calculadora, o que deseja fazer ?");
+      Console.WriteLine("");
+      Console.WriteLine("1 - Soma");
+      Console.WriteLine("2 - Subtração");
+      Console.WriteLine("3 - Divisão");
+      Console.WriteLine("4 - Multiplicação");
+      Console.WriteLine("0 - Sair");
+
+      Console.WriteLine("---------------");
+      Console.WriteLine("Selecione uma opção:");
+
+      int opcao = int.Parse(Console.ReadLine());
+
+      switch (opcao)
+      {
+        case 1: Soma(); break;
+        case 2: Substracao(); break;
+        case 3: Divisao(); break;
+        case 4: Multiplicacao(); break;
+        case 0: Sair(); break;
+        default: Menu(); break;
+      }
+
     }
 
     static void Soma()
@@ -26,7 +52,9 @@ namespace calculadora_csharp
 
       float resultado = num1 + num2;
       Console.WriteLine($"O resultado da soma é: {resultado}");
+
       Console.ReadKey();
+      Menu();
     }
 
     static void Substracao()
@@ -43,7 +71,9 @@ namespace calculadora_csharp
 
       float resultado = num1 - num2;
       Console.WriteLine($"O resultado da subtração é: {resultado}");
+
       Console.ReadKey();
+      Menu();
     }
 
     static void Divisao()
@@ -60,7 +90,9 @@ namespace calculadora_csharp
 
       float resultado = num1 / num2;
       Console.WriteLine($"O resultado da divisão é: {resultado}");
+
       Console.ReadKey();
+      Menu();
     }
 
     static void Multiplicacao()
@@ -75,6 +107,14 @@ namespace calculadora_csharp
 
       float resultado = num1 * num2;
       Console.WriteLine($"O resultado da multiplicação é: {resultado}");
+
+      Console.ReadKey();
+      Menu();
+    }
+
+    static void Sair()
+    {
+      System.Environment.Exit(0);
     }
   }
 }
