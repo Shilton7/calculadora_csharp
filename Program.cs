ebec773788681875a106eb3f94e91d60db96aca7
shilton <shilton.santiago@animaeducacao.com.br>
2020-10-22 09:39:37 -0300

    Substracao

--- Program.cs
+++ Program.cs
@@ -6,6 +6,12 @@ namespace calculadora_csharp
   {
     static void Main(string[] args)
     {
+      //Soma();
+      Substracao();
+    }
+
+    static void Soma()
+    {
       Console.Clear();
 
       Console.WriteLine("Digite o primeiro valor: ");
@@ -17,10 +23,25 @@ namespace calculadora_csharp
       Console.WriteLine("");
 
       float resultado = num1 + num2;
-      Console.WriteLine($"O resultado da soma é: {num1 + num2}");
       Console.WriteLine($"O resultado da soma é: {resultado}");
-      Console.WriteLine("O resultado da soma é: " + resultado);
+      Console.ReadKey();
+    }
 
+    static void Substracao()
+    {
+      Console.Clear();
+
+      Console.WriteLine("Digite o primeiro valor: ");
+      float num1 = float.Parse(Console.ReadLine());
+
+      Console.WriteLine("Digite o segundo valor: ");
+      float num2 = float.Parse(Console.ReadLine());
+
+      Console.WriteLine("");
+
+      float resultado = num1 - num2;
+      Console.WriteLine($"O resultado da soma é: {resultado}");
+      Console.ReadKey();
     }
   }
 }
