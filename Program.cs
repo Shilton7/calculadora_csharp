304acabf6445b52342fbd7138941885d4638d756
shilton <shilton.santiago@animaeducacao.com.br>
2020-10-22 09:42:12 -0300

    Divisao

--- Program.cs
+++ Program.cs
@@ -7,7 +7,8 @@ namespace calculadora_csharp
     static void Main(string[] args)
     {
       //Soma();
-      Substracao();
+      //Substracao();
+      Divisao();
     }
 
     static void Soma()
@@ -40,7 +41,24 @@ namespace calculadora_csharp
       Console.WriteLine("");
 
       float resultado = num1 - num2;
-      Console.WriteLine($"O resultado da soma é: {resultado}");
+      Console.WriteLine($"O resultado da subtração é: {resultado}");
+      Console.ReadKey();
+    }
+
+    static void Divisao()
+    {
+      Console.Clear();
+
+      Console.WriteLine("Digite o primeiro valor: ");
+      float num1 = float.Parse(Console.ReadLine());
+
+      Console.WriteLine("Digite o segundo valor: ");
+      float num2 = float.Parse(Console.ReadLine());
+
+      Console.WriteLine("");
+
+      float resultado = num1 / num2;
+      Console.WriteLine($"O resultado da divisão é: {resultado}");
       Console.ReadKey();
     }
   }
