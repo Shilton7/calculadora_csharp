f653da2330e8e9394a64477489994668fcfb9736
shilton <shilton.santiago@animaeducacao.com.br>
2020-10-22 09:30:35 -0300

    Soma

--- Program.cs
+++ Program.cs
@@ -6,7 +6,21 @@ namespace calculadora_csharp
   {
     static void Main(string[] args)
     {
-      Console.WriteLine("Hello Shilton!");
+      Console.Clear();
+
+      Console.WriteLine("Digite o primeiro valor: ");
+      float num1 = float.Parse(Console.ReadLine());
+
+      Console.WriteLine("Digite o segundo valor: ");
+      float num2 = float.Parse(Console.ReadLine());
+
+      Console.WriteLine("");
+
+      float resultado = num1 + num2;
+      Console.WriteLine($"O resultado da soma é: {num1 + num2}");
+      Console.WriteLine($"O resultado da soma é: {resultado}");
+      Console.WriteLine("O resultado da soma é: " + resultado);
+
     }
   }
 }
